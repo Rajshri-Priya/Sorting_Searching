df3b059971cb749ea161bc5d10a452101a5df047
rajshri-priya <rajshri.priya1708@.gmail.com>
2022-11-24 14:09:10 +0530

    binary sorting added

---
+++ binary_search/binary_search/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace binary_search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs\n 1.Binary search");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        binary binarysearch = new binary();
+                        binarysearch.Search("C:\\Users\\RJ EMPIRE\\Desktop\\New folder\\sorting\\binary_search\\TextFile1.txt");
+                        break;
+                }
+            }
+        }
+    }
+}
